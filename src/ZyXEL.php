63793a6945f2ecf46eeddda9fe08317f50177716
JW de Roos <trueskimmer@gmail.com>
2015-11-03 21:56:18 +0100

    Adds the Arp command

--- src/ZyXEL.php
+++ src/ZyXEL.php
@@ -2,6 +2,10 @@
 
 namespace jwdr\ZyXEL;
 
+use jwdr\ZyXEL\Output\ArpHost;
+use jwdr\ZyXEL\Output\Exceptions\FieldCountDoesNotMatchHeader;
+use jwdr\ZyXEL\Output\Exceptions\IsHeaderRecord;
+use jwdr\ZyXEL\Output\ArpHosts;
 use jwdr\ZyXEL\Output\LanHosts;
 class ZyXEL
 {
@@ -23,10 +27,26 @@ class ZyXEL
         return $this->connection->executeCommand('help');
     }
 
+    /**
+     * Returns a list of hosts that are known to have been connected to the ZyXEL
+     * There is no guarantee that any of these devices are currently connected
+     *
+     * @return LanHosts
+     */
     public function lanHosts()
     {
         $rawData = $this->connection->executeCommand('lanhosts show all');
-        unset($rawData[0]);
+
         return LanHosts::fromRawData($rawData);
+    }
+
+    /**
+     *
+     */
+    public function arp()
+    {
+        $rawData = $this->connection->executeCommand('arp show');
+
+        return ArpHosts::fromRawData($rawData);
     }
 }
